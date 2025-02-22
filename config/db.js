2867d5a5801d42ce873fb0e03b0f18ebed49ce5f
CoderRK <107743164+rajankumar4723@users.noreply.github.com>
2025-02-22 11:53:56 +0530

    backend env

--- config/db.js
+++ config/db.js
@@ -9,7 +9,6 @@ const connectDB = async () => {
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
 
     console.log("✅ MongoDB Connected Successfully");
