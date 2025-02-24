a1043ab2c8816a95b3c74411f99705ffce412c3b
CoderRK <107743164+rajankumar4723@users.noreply.github.com>
2025-02-24 22:39:29 +0530

    Done

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config({ path: "./database/config.env" });
 
 // ✅ Connect to Database and Start Server
 connectDB().then(() => {
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 8000;
 
     app.listen(PORT, () => {
         console.log(`🚀 Server is running on port: ${PORT}`);
