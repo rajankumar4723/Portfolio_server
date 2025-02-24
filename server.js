769885bc0c1e352249ee6416d95e1cdccf63cfc7
CoderRK <107743164+rajankumar4723@users.noreply.github.com>
2025-02-24 11:45:37 +0530

    Port is Done

--- server.js
+++ server.js
@@ -11,18 +11,22 @@ const startServer = async () => {
     try {
         await connectDB(); // Ensure database connection is successful
         console.log("Database connected successfully");
-        
+
         const app = express();
         app.use(express.json());
         app.use(cors());
 
         app.use('/api/contact', contactRoutes);
 
-        const PORT = process.env.PORT || 5000;
+        const port = process.env.PORT || 5000;
         const HOST = '0.0.0.0'; // Ensure Render detects the open port
 
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
+        app.get('/', (req, res) => {
+            res.send('Hello World!')
+        })
+
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
         });
     } catch (error) {
         console.error("Database connection failed:", error);
